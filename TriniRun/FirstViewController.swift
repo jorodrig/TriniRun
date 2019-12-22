cb20017bb8570381ac6d7a0f8b36d23fa38c517b
Joseph Rodrigues <joseph@pc-sg.com>
2019-12-22 15:45:48 -0500

    Initial Commit

---
+++ TriniRun/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TriniRun
+//
+//  Created by Joseph on 12/22/19.
+//  Copyright © 2019 Coconut Tech LLc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TriniRun/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TriniRun
+//
+//  Created by Joseph on 12/22/19.
+//  Copyright © 2019 Coconut Tech LLc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
