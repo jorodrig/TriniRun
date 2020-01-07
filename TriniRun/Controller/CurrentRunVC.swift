10ff926980d963b55e63d48c42da4d3df8f22311
Joseph Rodrigues <joseph@pc-sg.com>
2020-01-06 22:08:16 -0500

    Setup CurrentRunVC swift file and map to storyboard

---
+++ TriniRun/Controller/CurrentRunVC.swift
@@ -0,0 +1,19 @@
+//
+//  CurrentRunVC.swift
+//  TriniRun
+//
+//  Created by Joseph on 1/6/20.
+//  Copyright © 2020 Coconut Tech LLc. All rights reserved.
+//
+
+import UIKit
+
+class CurrentRunVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
