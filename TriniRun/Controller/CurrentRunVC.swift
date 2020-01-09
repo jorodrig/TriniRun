979d0fa5674e4e7c72bbadf3921525a578cde10a
Joseph Rodrigues <joseph@pc-sg.com>
2020-01-08 21:13:22 -0500

    Started slider button setup

--- TriniRun/Controller/CurrentRunVC.swift
+++ TriniRun/Controller/CurrentRunVC.swift
@@ -2,18 +2,52 @@
 //  CurrentRunVC.swift
 //  TriniRun
 //
-//  Created by Joseph on 1/6/20.
+//  Created by Joseph on 1/8/20.
 //  Copyright © 2020 Coconut Tech LLc. All rights reserved.
 //
 
 import UIKit
 
-class CurrentRunVC: UIViewController {
+class CurrentRunVC: LocationVC {  //inherits from LocationVC which inherits from UIViewController
 
+    @IBOutlet weak var swipeBGImageView: UIImageView!
+    
+    @IBOutlet weak var sliderImageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        /* swipeGesture is used to control the custom slider*/
+        let swipeGesture = UIPanGestureRecognizer(target: self, action: #selector(endRunSwiped(sender:)))
+        sliderImageView.addGestureRecognizer(swipeGesture) //Recognize slidegestures
+        sliderImageView.isUserInteractionEnabled = true  //allow user interaction
+        swipeGesture.delegate = self as? UIGestureRecognizerDelegate //must have
+        
     }
-
+    
+/* endRunSwiped is used to end the run action - user will swipe or slide left to right to end or use pause button*/
+    @objc func endRunSwiped(sender: UIPanGestureRecognizer){
+        /* the constants below are used to adjust the slider relative to its background view
+         not the main view controller.*/
+        let minAdjust: CGFloat = 80
+        let maxAdjust: CGFloat = 128
+        if let sliderView = sender.view{
+            if sender.state == UIGestureRecognizer.State.began || sender.state == UIGestureRecognizer.State.changed{
+                let translation = sender.translation(in: self.view)
+                if sliderView.center.x >= (swipeBGImageView.center.x - minAdjust) && sliderView.center.x <= (swipeBGImageView.center.x + maxAdjust){
+                    sliderView.center.x = sliderView.center.x + translation.x
+                    
+                } else if sliderView.center.x >= swipeBGImageView.center.x + maxAdjust{
+                    sliderView.center.x = swipeBGImageView.center.x + maxAdjust
+                    //End Run Code goes here
+                    dismiss(animated: true, completion: nil)  //return to the main view as run has ended
+                } else{
+                    sliderView.center.x = swipeBGImageView.center.x - minAdjust
+                }
+                sender.setTranslation(CGPoint.zero, in: self.view)
+            }
+        }
+        
+    }
 
 }
