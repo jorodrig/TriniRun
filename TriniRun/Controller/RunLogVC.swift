d1805e13db8e3128c27f8cb8fb77d99542c53926
Joseph Rodrigues <joseph@pc-sg.com>
2020-02-08 08:19:30 -0500

    Added output to Log View

--- TriniRun/Controller/RunLogVC.swift
+++ TriniRun/Controller/RunLogVC.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class RunLogVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self           //required when extending class below
+        tableView.dataSource = self         //required when extending class below
     }
 
 
 }
 
+extension RunLogVC: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Run.getAllRuns()?.count ?? 0 //optional since inititally we have no runs. if no runs return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "RunLogCell") as? RunLogCell{
+            guard let run = Run.getAllRuns()?[indexPath.row] else {
+                return RunLogCell()
+            }
+            cell.configure(run: run)
+            return cell
+        } else {
+            return RunLogCell()
+        }
+    }
+}
---
+++ TriniRun/View/RunLogCell.swift
@@ -0,0 +1,32 @@
+//
+//  RunLogCell.swift
+//  TriniRun
+//
+//  Created by Joseph on 2/8/20.
+//  Copyright © 2020 Coconut Tech LLc. All rights reserved.
+//
+
+import UIKit
+
+class RunLogCell: UITableViewCell {
+
+    @IBOutlet weak var runDurationLbl: UILabel!
+    @IBOutlet weak var totalDIstanceLbl: UILabel!
+    @IBOutlet weak var averagePaceLbl: UILabel!
+    @IBOutlet weak var dateLbl: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    /* Function to configure the table view Cell */
+    func configure(run: Run) {
+        runDurationLbl.text = run.duration.formatTimeDurationToString() //we created the formatTimedurationToString override methog
+        totalDIstanceLbl.text = "\run.distance.metersToMiles(places: 2)) mi"
+        averagePaceLbl.text = run.pace.formatTimeDurationToString()
+        dateLbl.text = run.date.getDateString()
+        
+    }
+
+}
